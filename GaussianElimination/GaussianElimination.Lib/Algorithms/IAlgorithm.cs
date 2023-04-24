39a83140f6231f57c1a1e57a65306e939bb36131
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-25 00:33:27 +0200

    + IAlgorithm

---
+++ GaussianElimination/GaussianElimination.Lib/Algorithms/IAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using GaussianElimination.Lib.Core;
+
+namespace GaussianElimination.Lib.Algorithms;
+
+public interface IAlgorithm<T> where T: 
+    IMultiplyOperators<T, T, T>, ISubtractionOperators<T, T, T>,
+    IDivisionOperators<T, T, T>, IAdditionOperators<T, T, T>
+{
+    public static abstract T[] Solve(Matrix<T> coefficients, T[] values);
+}
