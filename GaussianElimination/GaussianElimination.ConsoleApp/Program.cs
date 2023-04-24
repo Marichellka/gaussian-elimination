dc49d9f315690dd79ff5b3751acae168d1497015
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-25 00:34:21 +0200

    + console app program with testing example

---
+++ GaussianElimination/GaussianElimination.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using GaussianElimination.Lib.Algorithms;
+using GaussianElimination.Lib.Core;
+
+Matrix<float> coeffs = new Matrix<float>(new float[][]
+{
+    new float[] { 2, 1, -1 }, 
+    new float[] { -3, -1, 2}, 
+    new float[] { -2, 1, 2}
+});
+
+float[] values = new float[]{8, -11, -3};
+float[] result = SequentialAlgorithm<float>.Solve(coeffs, values);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine($"x{i + 1} = {result[i]}");
+}
