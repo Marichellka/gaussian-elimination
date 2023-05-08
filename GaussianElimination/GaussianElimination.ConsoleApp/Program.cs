fe1289f94cf281324b69d8d00c9a2cd7fe4ea837
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-05-08 20:52:54 +0200

    add test for correctness

--- GaussianElimination/GaussianElimination.ConsoleApp/Program.cs
+++ GaussianElimination/GaussianElimination.ConsoleApp/Program.cs
@@ -3,3 +3,4 @@ using GaussianElimination.Lib.Core;
 
 int[] sizes = new[] { 100, 500, 1000, 1500, 2000, 2500 };
 Tester.TestMultiple(new SequentialAlgorithm(), sizes);
+Tester.TestCorrectness(new SequentialAlgorithm());
--- GaussianElimination/GaussianElimination.Lib/Core/Tester.cs
+++ GaussianElimination/GaussianElimination.Lib/Core/Tester.cs
@@ -21,4 +21,31 @@ public class Tester
             Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
         }
     }
+
+    public static void TestCorrectness(IAlgorithm algorithm)
+    {
+        Matrix coefficients = new Matrix(new float[][]
+        {
+            new float[] { 11, 13, -4, 8 },
+            new float[] { 1, 9, -5, -3 },
+            new float[] { -21, -12, 5, -1 },
+            new float[] { 4, 31, 7, 3 },
+        });
+        float[] values = new float[] { -4, 8, -8, 17 };
+        double[] answer = new double[] { 0.22, 0.58, 0.41, -1.54 };
+            
+        float[] result = algorithm.Solve(coefficients, values);
+        bool isCorrect = true;
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine($"x{i+1} = {result[i]}");
+            if (Math.Abs(result[i] - answer[i]) > 0.01)
+            {
+                isCorrect = false;
+            }
+        }
+
+        Console.WriteLine($"Result is correct: {isCorrect}");
+    }
 }
